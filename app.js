e53ab520531fa5ba21c4d5878bb7ff1c636e81d6
jaswant2111058 <jaswant2111058@akgec.ac.in>
2022-12-20 23:41:01 +0530

    final touch

--- app.js
+++ app.js
@@ -31,8 +31,22 @@ const static3 = path.join(__dirname,"./public")
 function isLoggedIn(req, res, next) {
   req.user ? next() : res.redirect("/login");
 }
+function emailverification(req,res,next)
+{
+  var email = req.user.email;
+  let l= email.length;
+  let mail="";
+  for(let i=l;i>l-12;i--)
+  {
+      mail = email[i]+mail;
+  }
+  if(mail=="akgec.ac.inundefined")
+  {
+    next();
+  }
+  else res.send("you are not the part of akgec.ac.in")
 
-
+}
 
 app.use(session({ secret: 'jassi', resave: false, saveUninitialized: true }));
 app.use(passport.initialize());
@@ -61,7 +75,14 @@ app.get( '/auth/google/callback',
 );
 
 
-  app.get('/filldetails',isLoggedIn, async (req,res)=>{
+  app.get('/filldetails',isLoggedIn,emailverification, async (req,res)=>{
+
+            
+
+
+
+
+
 
     // const adnew = await schema.findOne({leader_email:req.user.email});
     //       if(adnew){
@@ -75,12 +96,12 @@ app.get( '/auth/google/callback',
     //       }
       res.sendFile(static1+"/filldetail.html");
   })
-app.post('/filldetail', isLoggedIn, async (req,res)=>
+app.post('/filldetail', isLoggedIn,emailverification, async (req,res)=>
 {
 
   const team_member_email=req.body.tmname;
   const team_member_name=req.body.tmemail;
-  const detail= {leader_name:req.user.displayName,leader_email:req.user.email,img_url:req.user.picture,team_member_email:team_member_email,team_member_name:team_member_name,payment_status:"unseccessful",razorpay_order_id:"none",razorpay_payment_id:"none",} 
+  const detail= {leader_name:req.user.displayName,leader_email:req.user.email,img_url:req.user.picture,team_member_email:team_member_email,team_member_name:team_member_name,payment_status:"unseccessful",razorpay_order_id:"none",razorpay_payment_id:"none",razorpay_signature:"none"} 
   
     try{
   const usr = new schema(detail);
@@ -96,7 +117,7 @@ app.post('/filldetail', isLoggedIn, async (req,res)=>
 
 
 
-app.post('/review', isLoggedIn,async (req, res) => {
+app.post('/review', isLoggedIn,emailverification,async (req, res) => {
 
    //res.send(`<html><h1>DETAIL<h1> <br><img src="${adnew.img_url}" <h5>LEARDER NAME : ${adnew.leader_name}</h3><br><h3>LEARDER EMAIL : ${adnew.leader_email}</h5><br><h5>TEAM MEMBER EMAIL : ${adnew.team_member_email}</h5><br><h5>TEAM MEMBER NAME : ${adnew.team_member_name}</h5><br><h5>PAYMENT : ${adnew.payment_status}</h5><html>`)
   // res.sendFile(static1+'/filldetail.html');
@@ -106,7 +127,7 @@ app.post('/review', isLoggedIn,async (req, res) => {
    res.send(adnew);
  });
 
-app.get('/create/orderId', isLoggedIn,  async (req, res) => {
+app.get('/create/orderId', isLoggedIn,emailverification,  async (req, res) => {
   
   let options = {
     amount: 2000,  // amount in the smallest currency unit
@@ -117,9 +138,10 @@ app.get('/create/orderId', isLoggedIn,  async (req, res) => {
     console.log(order);
    // res.send({orderId:order.id})
     try{
-    const adnew = await schema.updateOne({leader_email:req.user.email},{razorpay_order_id:order.id});
+     await schema.updateOne({leader_email:req.user.email},{razorpay_order_id:order.id});
+     const adnew = await schema.findOne({leader_email:req.user.email});
     //res.send(adnew);
-    res.render("payment",{order:order.id});
+    res.render("payment",{order:order.id,adnew});
     }
     catch(e){
     console.log("error");
@@ -141,18 +163,17 @@ app.post("/api/payment/verify", async (req,res)=>{
                                    console.log("sig generated " ,expectedSignature);
    var response = {"signatureIsValid":"false"}
    if(expectedSignature === req.body.razorpay_signature)
-    var response={"signatureIsValid":"true"}
-if(response.signatureIsValid==true)
+   
  {
-  const keep = await schema.updateOne({leader_email:req.user.email},{payment_status:"successfull"});
-  //const adnew = await schema.findOne({leader_email:req.user.email});
-  console.log(keep);
-  res.send(keep);
+  const keep = await schema.updateOne({leader_email:req.user.email},{payment_status:"successfull",razorpay_signature:"verified",razorpay_payment_id:req.body.razorpay_payment_id});
+  const adnew = await schema.findOne({leader_email:req.user.email});
+ 
+  res.send(adnew);
  }
       else 
      { const keep = await schema.updateOne({leader_email:req.user.email},{payment_status:"unsuccessfull"});
-     console.log(keep)
-     res.send(keep);
+   //  console.log(req.body.razorpay_order_id,req.body.razorpay_payment_id,req.body.razorpay_signature);
+     res.send("payment is not veryfied");
 }
    
   
